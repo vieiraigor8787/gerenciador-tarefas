5a659e8f3e256dac0d76f546f6dbab597ede3296
Igor Cerqueira <igor_vc@hotmail.com>
2019-05-18 18:42:21 -0300

    new directive_tarefa-concluida

--- gerenciador-tarefas/src/app/tarefas/tarefas.module.ts
+++ gerenciador-tarefas/src/app/tarefas/tarefas.module.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 
-import { TarefaService } from './shared';
+import { TarefaService, TarefaConcluidaDirective } from './shared';
 import { ListarTarefasComponent } from './listar';
 import { CadastrarTarefaComponent } from './cadastrar';
 import { EditarTarefaComponent } from './editar';
@@ -12,7 +12,8 @@ import { EditarTarefaComponent } from './editar';
   declarations: [
     ListarTarefasComponent,
     CadastrarTarefaComponent,
-    EditarTarefaComponent
+    EditarTarefaComponent,
+    TarefaConcluidaDirective
   ],
   imports: [
     CommonModule,
